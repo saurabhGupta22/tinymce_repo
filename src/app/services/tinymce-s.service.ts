7565a6381bc16cce1e60f8cdd9f3c5ff3d511f1c
Saurabh Gupta <saurabh.gupta@Triconinfotech.com>
2019-12-26 12:21:18 +0530

    changes in file picker callback

--- src/app/services/tinymce-s.service.ts
+++ src/app/services/tinymce-s.service.ts
@@ -55,6 +55,49 @@ export class TinymceSService {
       })
     };
 
+
+
+    var mediaFilePicker = function (callback, value, meta) {
+
+      tinymce.activeEditor.windowManager.openUrl({
+
+        title: 'Value and Variable',
+        url: '../assets/demo/tinyDemo_media.html',
+        buttons: [
+          {
+            type: "custom",
+            name: "insert-and-close",
+            text: "Insert and Close",
+            primary: true,
+            align: "end"
+          },
+          {
+            type: "cancel",
+            name: "cancel",
+            text: "Close Dialog"
+          }
+
+        ],
+
+        height: 350,
+        width: 500,
+
+        onAction: function (instance, trigger) {
+          instance.sendMessage({
+            mceAction: "customInsertAndClose3"
+          });
+        },
+      })
+
+      // command to insert the image URL in the image tool source textbox
+      ed.addCommand("mediaCommand", function (ui, value) {
+
+        if (meta.filetype == 'media') {
+          callback(value.videoValue);
+        }
+      })
+    };
+
     // tinymce.wirisUrl = 'https://wiris-tinymce-dev.mheducation.com/4.8.0.1377';
 
     // tinymce initialization
@@ -71,19 +114,32 @@ export class TinymceSService {
         'insertdatetime media nonbreaking save table contextmenu directionality',
         'emoticons template paste textcolor colorpicker textpattern imagetools'],
 
-      toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image media ',
       toolbar2: 'print preview | forecolor backcolor emoticons',
       toolbar3: 'valueButton variableButton',
 
-      file_picker_types: "image",
 
-      file_picker_callback: function (callback, value, meta) {
-        imageFilePicker(callback, value, meta);
-      },
-
+      file_picker_types: " file image media",
+      
       image_advtav: true,
       paste_data_images: true,
       automatic_uploads: true,
+      video_template_callback: function(data) {
+        console.log(data)
+        return '<video width="' + data.width + '" height="' + data.height + '"' + (data.poster ? ' poster="' + data.poster + '"' : '') + ' controls="controls">\n' + '<source src="../../assets/demo/assets/resources/mov_bbb.mp4"' + (data.source1mime ? ' type="' + data.source1mime + '"' : '') + ' />\n' + (data.source2 ? '<source src="' + data.source2 + '"' + (data.source2mime ? ' type="' + data.source2mime + '"' : '') + ' />\n' : '') + '</video>';
+      },
+
+      file_picker_callback: function (callback, value, meta) {
+        if (meta.filetype == 'image') {
+          imageFilePicker(callback, value, meta);
+        }
+        if(meta.filetype == 'media'){
+          mediaFilePicker(callback,value,meta);
+        }
+
+      },
+      media_live_embeds: true,
+
 
       setup: function (editor) {
         ed = editor;
